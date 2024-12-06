acc1e87c0277c399b807a6a4da796a2d35868605
arjunchuhan6 <arjunchuhan6@gmail.com>
2024-12-07 05:14:56 +0530

    Project start and front-end initiall

---
+++ front-end/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline"><a href="https://www.youtube.com">Welcome To Youtube Clone Project</a></h1>
+    </>
+  )
+}
+
+export default App
